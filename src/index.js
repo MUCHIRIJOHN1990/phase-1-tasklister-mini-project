ac1b39e49831652907b1ea62898c58415465ede1
MUCHIRI90 <john.muchiri@student.moringaschool.com>
2024-07-17 07:19:05 +0300

    Add task creation functionality to ToDo list app
    - Implement task addition feature: users can now enter tasks into an input field and submit them to
      see the tasks displayed as a list on the page.
    - Enable editing of task names directly in the list by making the task text content editable.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputEl = document.querySelector("input#new-task-description");
+  const createTaskFormEl = document.querySelector("form#create-task-form");
+  const taskListEl = document.querySelector("ul#tasks");
+
+  createTaskFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const inputElValue = inputEl.value;
+
+    const listItemEl = document.createElement("li");
+
+    const spanEl = document.createElement("span");
+    spanEl.textContent = inputElValue;
+    spanEl.contentEditable = true;
+    spanEl.title = "Click to edit!";
+
+    listItemEl.appendChild(spanEl);
+    taskListEl.appendChild(listItemEl);
+  });
 });
